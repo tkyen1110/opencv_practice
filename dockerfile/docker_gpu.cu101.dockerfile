9b91f5df48350aacdd3f9f5b6b7a232a34fb2c33
Tk.Yen <tk.yen@advantech.com.tw>
2021-03-09 10:00:47 +0800

    Add dockerfile folder

---
+++ dockerfile/docker_gpu.cu101.dockerfile
@@ -0,0 +1,78 @@
+FROM ufoym/deepo:all-py36-cu101
+
+ARG USER=adev
+ARG UID=1000
+ARG GID=1000
+
+ENV HADOOP_VERSION=2.7.2
+ENV DEBIAN_FRONTEND=noninteractive
+# LABEL HADOOP_VERSION=2.7.2
+
+RUN DEBIAN_FRONTEND=noninteractive && \
+    apt-get -y update && \
+    apt-get -y install --no-install-recommends curl \
+        unzip \
+        automake \
+        openjdk-8-jdk \
+        openssh-server \
+        openssh-client \
+        lsof \
+        libcupti-dev && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN wget -qO- http://archive.apache.org/dist/hadoop/common/hadoop-${HADOOP_VERSION}/hadoop-${HADOOP_VERSION}.tar.gz | \
+    tar xz -C /usr/local && \
+    mv /usr/local/hadoop-${HADOOP_VERSION} /usr/local/hadoop
+
+ENV JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64 \
+    HADOOP_INSTALL=/usr/local/hadoop \
+    NVIDIA_VISIBLE_DEVICES=all
+
+ENV HADOOP_PREFIX=${HADOOP_INSTALL} \
+    HADOOP_BIN_DIR=${HADOOP_INSTALL}/bin \
+    HADOOP_SBIN_DIR=${HADOOP_INSTALL}/sbin \
+    HADOOP_HDFS_HOME=${HADOOP_INSTALL} \
+    HADOOP_COMMON_LIB_NATIVE_DIR=${HADOOP_INSTALL}/lib/native \
+    HADOOP_OPTS="-Djava.library.path=${HADOOP_INSTALL}/lib/native"
+
+ENV PATH=/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:${HADOOP_BIN_DIR}:${HADOOP_SBIN_DIR} \
+    LD_LIBRARY_PATH=/usr/local/nvidia/lib:/usr/local/nvidia/lib64:/usr/local/cuda/lib64:/usr/local/cuda/targets/x86_64-linux/lib/stubs:${JAVA_HOME}/jre/lib/amd64/server
+
+# Setup Jupyter
+# WORKDIR /app
+
+# COPY vendor ./vendor
+COPY requirements.txt .
+RUN pip install --upgrade --force-reinstall -r requirements.txt 
+    # --no-index --find-links=./vendor
+
+# # Setup path
+# RUN mkdir /root/AFS 
+# RUN echo "cd /root/AFS" >> /root/.bashrc 
+# WORKDIR /root/AFS
+
+# # Setup tensorboard
+# RUN mkdir /root/AFS/tensorboard
+
+
+# Set the home directory to our user's home.
+ENV USER=$USER
+ENV HOME="/home/$USER"
+
+RUN echo "Create $USER account" &&\
+    # Create the home directory for the new $USER
+    mkdir -p $HOME &&\
+    # Create an $USER so our program doesn't run as root.
+    groupadd -r -g $GID $USER &&\
+    useradd -r -g $USER -G sudo -u $UID -d $HOME -s /sbin/nologin -c "Docker image user" $USER &&\
+    # Set root user no password
+    mkdir -p /etc/sudoers.d &&\
+    echo "$USER ALL=(root) NOPASSWD:ALL" > /etc/sudoers.d/$USER && \
+    chmod 0440 /etc/sudoers.d/$USER && \
+    # Chown all the files to the $USER
+    chown -R $USER:$USER $HOME
+
+# Change to the $USER
+WORKDIR $HOME
+USER $USER
---
+++ dockerfile/dockerfile_opencv.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+# Color
+NC='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+
+# Absolute path to this script.
+# e.g. /home/ubuntu/opencv_practice/dockerfile/dockerfile_opencv.sh
+SCRIPT=$(readlink -f "$0")
+
+# Absolute path this script is in.
+# e.g. /home/ubuntu/opencv_practice/dockerfile
+SCRIPT_PATH=$(dirname "$SCRIPT")
+
+# Absolute path to the opencv path
+# e.g. /home/ubuntu/opencv_practice
+HOST_DIR_PATH=$(dirname "$SCRIPT_PATH")
+echo "HOST_DIR_PATH  = "$HOST_DIR_PATH
+
+# Host directory name
+IFS='/' read -a array <<< "$HOST_DIR_PATH"
+HOST_DIR_NAME="${array[-1]}"
+echo "HOST_DIR_NAME  = "$HOST_DIR_NAME
+
+
+if [ "$2" == "" ]
+then
+    VERSION="v1.0"
+else
+    VERSION=$2
+fi
+echo "VERSION        = "$VERSION
+
+IMAGE_NAME="opencv_practice:$VERSION"
+CONTAINER_NAME="opencv_practice_$VERSION"
+echo "IMAGE_NAME     = "$IMAGE_NAME
+echo "CONTAINER_NAME = "$CONTAINER_NAME
+
+IFS=$'\n'
+function Fun_EvalCmd()
+{
+    cmd_list=$1
+    i=0
+    for cmd in ${cmd_list[*]}
+    do
+        ((i+=1))
+        printf "${GREEN}${cmd}${NC}\n"
+        eval $cmd
+    done
+}
+
+
+if [ "$1" == "build" ]
+then
+    export GID=$(id -g)
+
+    lCmdList=(
+                "docker build \
+                    --build-arg USER=$USER \
+                    --build-arg UID=$UID \
+                    --build-arg GID=$GID \
+                    -f docker_gpu.cu101.dockerfile \
+                    -t $IMAGE_NAME ."
+             )
+    Fun_EvalCmd "${lCmdList[*]}"
+
+elif [ "$1" = "run" ]
+then
+    HOST_API_PORT="80"
+
+    lCmdList=(
+                "docker run --gpus all -itd \
+                    --privileged \
+                    --name $CONTAINER_NAME \
+                    -v $HOST_DIR_PATH:/home/$USER/$HOST_DIR_NAME \
+                    -v /tmp/.X11-unix:/tmp/.X11-unix \
+                    -v /etc/localtime:/etc/localtime:ro \
+                    --mount type=bind,source=$SCRIPT_PATH/.bashrc,target=/home/$USER/.bashrc \
+                    -p $HOST_API_PORT:8888 \
+                    $IMAGE_NAME /bin/bash" \
+                "docker exec -it $CONTAINER_NAME /bin/bash"
+             )
+    Fun_EvalCmd "${lCmdList[*]}"
+
+elif [ "$1" = "exec" ]
+then
+    lCmdList=(
+                "docker exec -it $CONTAINER_NAME /bin/bash"
+             )
+    Fun_EvalCmd "${lCmdList[*]}"
+
+elif [ "$1" = "start" ]
+then
+    lCmdList=(
+                "docker start -ia $CONTAINER_NAME"
+             )
+    Fun_EvalCmd "${lCmdList[*]}"
+
+elif [ "$1" = "attach" ]
+then
+    lCmdList=(
+                "docker attach $CONTAINER_NAME"
+             )
+    Fun_EvalCmd "${lCmdList[*]}"
+
+elif [ "$1" = "stop" ]
+then
+    lCmdList=(
+                "docker stop $CONTAINER_NAME"
+             )
+    Fun_EvalCmd "${lCmdList[*]}"
+
+elif [ "$1" = "rm" ]
+then
+    lCmdList=(
+                "docker rm $CONTAINER_NAME"
+             )
+    Fun_EvalCmd "${lCmdList[*]}"
+
+elif [ "$1" = "rmi" ]
+then
+    lCmdList=(
+                "docker rmi $IMAGE_NAME"
+             )
+    Fun_EvalCmd "${lCmdList[*]}"
+
+fi
